4b829db83b7b3604c5e0c7f1137c244b5a49b1f5
ahmed-hassan0020051 <ahmedhassan0020051@gmail.com>
2023-10-03 22:11:13 +0500

    ok

---
+++ assets/js/data.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:false,
+        dots:true,
+        items:4,
+        autoplay:true,
+        autoplayTimeout:1000
+        
+    })
+
+})
